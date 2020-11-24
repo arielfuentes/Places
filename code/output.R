409050ab44f127b569627668b9d86f67915a371b
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2020-11-24 17:41:17 -0300

    Update output.R

--- code/output.R
+++ code/output.R
@@ -1,2 +1,9 @@
 library(rmarkdown)
-render(input = "code/lugares.Rmd", output_dir = "output", output_file = "mall")
+Region <- c("Región de Antofagasta", "Región del Bío-Bío", "Región de La Araucanía")
+lapply(X = Region, FUN = function(x) tryCatch(render(input = "code/lugares.Rmd", 
+                                            output_dir = "output", 
+                                            output_file = paste0("mall", x), 
+                                            encoding = "utf-8"), 
+                                            error = function(e) NULL
+                                            )
+       )
